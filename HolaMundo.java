f750a63aa78bc7e83d6e401fd198b45ddf2f28fe
ivannf <alum.inegronf.2021@iesalixar.org>
2021-09-27 14:09:48 +0200

    comentarios añadidos al código

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,3 +1,8 @@
+/**
+* Muestra por pantalla la frase "¡Hola mundo!"
+*
+*@author ivannf
+*/
 public class HolaMundo {
      public static void main(String[] args) {
   	System.out.println("¡Hola mundo!");
