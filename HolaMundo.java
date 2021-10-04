cf8faf556d60e17263c424b2ca716ed08621d471
ivannf <alum.inegronf.2021@iesalixar.org>
2021-10-04 13:08:36 +0200

    Colores definidos

--- HolaMundo.java
+++ HolaMundo.java
@@ -5,6 +5,9 @@
 */
 public class HolaMundo {
      public static void main(String[] args) {
+  	String naranja = "\033[33m";
+  	String azul = "\033[34m";
+  	
   	System.out.println("¡Hola mundo!");
   	System.out.println("Aquí estamos haciendo pruebas con Git.");
      }
